71e81a3404a0e6f8c8bda21a82bb368061f85c49
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-03-19 20:55:23 +0530

    Improved the constructFace function

--- 3/GouraudShading/GouraudShading.cpp
+++ 3/GouraudShading/GouraudShading.cpp
@@ -25,14 +25,19 @@ void avgNormal (int normals[][3], int avgNormal[3])
 }
 
 /* Function to construct a polygon given the vertices. 
- * This function generates a face given its vertices. 
+ * This function generates a face given its vertices and the intensities at those 
+ * points.
  */
-void constructFace (double vertices[4][3], double intensities[4])
+void constructFace (double vertices[4][3], double intensities[4][3])
 {
 	glBegin (GL_POLYGON);	
+	glColor3dv  (intensities[0]);
 	glVertex3dv (vertices[0]);
+	glColor3dv  (intensities[1]);
 	glVertex3dv (vertices[1]);
+	glColor3dv  (intensities[2]);
 	glVertex3dv (vertices[2]);
+	glColor3dv  (intensities[3]);
 	glVertex3dv (vertices[3]);
 	glEnd ();
 }
