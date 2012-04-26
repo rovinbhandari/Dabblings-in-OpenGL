2063cbb0e4c40618ebe056b224e01ca4a5a81741
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-04-26 21:23:28 +0530

    Some modifications.

--- 4/another_take/general.h
+++ 4/another_take/general.h
@@ -1,34 +1,16 @@
-typedef struct _Vertex{
+#ifndef GENERAL_INCLUDED
+#define GENERAL_INCLUDED
+
+typedef struct _Vertex
+{
   float x, y, z;
-}Vertex;
+} Vertex;
 
-Vertex vertex(float x, float y, float z){
-  Vertex a;
-  a.x=x; a.y=y; a.z=z;
-  return a;
-}
+Vertex vertex(float x, float y, float z);
+Vertex subVertex(Vertex a, Vertex b);
+float dotProduct(Vertex a, Vertex b);
+Vertex crossProduct(Vertex a, Vertex b);
+float modVertex(Vertex a);
+Vertex normalizeVertex(Vertex a);
 
-Vertex subVertex(Vertex a, Vertex b){
-  return vertex(a.x-b.x, a.y-b.y, a.z-b.z);
-}
-
-float dotProduct(Vertex a, Vertex b){
-  return a.x*b.x+a.y*b.y+a.z*b.z;
-}
-
-Vertex crossProduct(Vertex a, Vertex b){
-  Vertex c;
-  c.x=a.y*b.z-a.z*b.y;
-  c.y=a.z*b.x-a.x*b.z;
-  c.z=a.x*b.y-a.y*b.x;
-  return c;
-}
-
-float modVertex(Vertex a){
-  return sqrt(pow(a.x,2)+pow(a.y,2)+pow(a.z,2));
-}
-
-Vertex normalizeVertex(Vertex a){
-  float mod=modVertex(a);
-  return vertex(a.x/mod, a.y/mod, a.z/mod);
-}
+#endif // end of file
