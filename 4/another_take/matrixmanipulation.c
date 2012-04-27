0d56de0755ccbb8f41800ee5d82267f2d4b19797
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-04-27 20:49:21 +0530

    Changes to matrixmanipulation
    Variable names and other changes inside functions.
    Comments added.

--- 4/another_take/matrixmanipulation.c
+++ 4/another_take/matrixmanipulation.c
@@ -1,12 +1,17 @@
 #include <matrixmanipulation.h>
+#include <string.h>
 
-MatrixStack *mStack = NULL;
-MatrixStack *pStack = NULL;
+MatrixStack *mStack = NULL;   // Modelview stack
+MatrixStack *pStack = NULL;   // Projection view stack
 MatrixStack **stack = &mStack;
 
-int matrixMode=MODELVIEW;
+int matrixMode = MODELVIEW;   // Default mode set to Modelview.
 
-float identMatrix[16] = 
+#define SIZEOF_MATRIX sizeof (float) * 16 
+#define index(i,j) i * 4 + j
+
+/* 4 x 4 Identity matrix (defined as 1D array) */
+float identitymatrix[16] = 
 {
 	1.0, 0.0, 0.0, 0.0,
 	0.0, 1.0, 0.0, 0.0,
@@ -14,142 +19,203 @@ float identMatrix[16] =
 	0.0, 0.0, 0.0, 1.0
 };
 
-void setMatrixMode(int a)
+void setMatrixMode (MatrixMode mode)
 {
-  matrixMode=a;
-  stack=(matrixMode==MODELVIEW)?(&mStack):(&pStack);
+  matrixMode = mode;
+  stack = (matrixMode == MODELVIEW) ? (&mStack) : (&pStack);
 }
 
-int getMatrixMode()
+int getMatrixMode ()
 {
   return matrixMode;
 }
 
-MatrixStack *initMatrix()
+MatrixStack *initMatrix ()
 {
   MatrixStack *temp;
-  int i;
+
   //Free any previous stack memory
-  while(temp=*stack){
-    *stack=temp->prev;
-    free(temp);
+  while(temp = *stack)
+  {
+    *stack = temp->prev;
+    free (temp);
   }
+  
   //Add an identity matrix as the top
-  *stack=(MatrixStack *)malloc(sizeof(MatrixStack));
-  for(i=0;i<16;i++){
-    (*stack)->topMatrix[i]=identMatrix[i];
-  }
-  (*stack)->prev=NULL;
+  *stack = (MatrixStack*) malloc (sizeof (MatrixStack));
+  memcpy ((*stack)->topMatrix, identitymatrix, SIZEOF_MATRIX);
+  (*stack)->prev = NULL;
   return *stack;
 }
 
 MatrixStack *pushMatrix()
 {
   MatrixStack *temp;
-  int i;
-  temp=(MatrixStack *)malloc(sizeof(MatrixStack));
-  for(i=0;i<16;i++){
-    temp->topMatrix[i]=(*stack)->topMatrix[i];
-  }
-  temp->prev=*stack;
-  *stack=temp;
+  temp = (MatrixStack*) malloc (sizeof (MatrixStack));
+
+  // Copy the top of the stack
+  memcpy (temp->topMatrix, (*stack)->topMatrix, SIZEOF_MATRIX);
+
+  temp->prev = *stack;
+  *stack = temp;
   return *stack;
 }
 
 MatrixStack *popMatrix()
 {
   MatrixStack *temp;
-  int i;
-  if(temp=*stack){
-    *stack=(*stack)->prev;
-    free(temp);
-  } else {
-    printf("Matrix not initialized.\n");
+
+  if(temp = *stack)
+  {
+    // If the stack's not empty. Delete the top most element.
+    *stack = (*stack)->prev;
+    free (temp);
+  } 
+  else 
+  {
+    printf ("Matrix not initialized.\n");
   }
   return NULL;
 }
 
-MatrixStack *multMatrix(float *matrixB)
+MatrixStack *multMatrix (float *matrixB)
 {
   int i, j, k;
-  float matrixC[16], *matrixA=(*stack)->topMatrix;
-  for(i=0;i<4;i++){
-    for(j=0;j<4;j++){
-      matrixC[i*4+j]=matrixA[i*4]*matrixB[j] + matrixA[i*4+1]*matrixB[4+j] + matrixA[i*4+2]*matrixB[8+j] +matrixA[i*4+3]*matrixB[12+j];
+  float matrixC[16], *matrixA = (*stack)->topMatrix;
+
+  for(i = 0; i < 4; i++)
+  {
+    for(j = 0; j < 4; j++)
+    {
+      // Fill the (i,j)th element of the matrix.
+      matrixC[i * 4 + j] = 0;                         
+      for (k = 0; k < 4; k++)
+      {
+        matrixC[index (i,j)] += matrixA[index (i,k)] * matrixB[index (k,j)];
+      }
+
     }
   }
-  for(i=0;i<16;i++){
-    matrixA[i]=matrixC[i];
-  }
+  
+  memcpy (matrixA, matrixC, SIZEOF_MATRIX);
   return *stack;
 }
 
-MatrixStack *translate(float x,float y,float z)
+MatrixStack *translate(float x, float y, float z)
 {
-  float m[16];
+  float matrix[16];
   int i;
- 
-  for(i=0;i<16;i++){
-    m[i]=0;
+  
+  // Set all matrix elements to zero
+  memset (matrix, 0, SIZEOF_MATRIX);
+
+  // Set elements in the last column.
+  matrix[index (3,0)] = x;
+  matrix[index (3,1)] = y;
+  matrix[index (3,2)] = z;
+
+  // Set diagonal elements.
+  for (i = 0; i < 4; i++)
+  {
+    matrix[index (i,i)] = 1;
   }
-  m[12] = x;
-  m[13] = y;
-  m[14] = z;
-  m[0]=m[5]=m[10]=m[15] = 1;
-  return multMatrix(m);
+
+  // Multiply the matrix with that on the top of the stack.
+  return multMatrix (matrix);
 }
 
-MatrixStack *scale(float x,float y,float z)
+MatrixStack *scale(float x, float y, float z)
 {
-  float m[16];
-  int i;
+  float matrix[16];
  
-  for(i=0;i<16;i++){
-    m[i]=0;
-  }
-  m[0] = x;
-  m[5] = y;
-  m[10] = z;
-  m[15]=1;
-  return multMatrix(m);
+  // Set all elements to zero.
+  memset (matrix, 0, SIZEOF_MATRIX);
+
+  // Set the diagonal elements
+  matrix[index (0,0)] = x;
+  matrix[index (1,1)] = y;
+  matrix[index (2,2)] = z;
+  matrix[index (3,3)] = 1;
+
+  // Multiply the matrix with that on the top of the stack.
+  return multMatrix (matrix);
 }
 
-MatrixStack *rotate(float degree,int x, int y,int z)
+MatrixStack *rotate(float degree, int x, int y, int z)
 {
-  float m[16];
-  float angle = degree*3.1416/180;
-  int i;
+  float matrix[16];
+  float angle = degree * 3.1416 / 180;
  
-  for(i=0;i<15;i++){
-    m[i]=0;
-  }
-  m[15]=1.0;
+  // Set elements to zero.
+  memset (matrix, 0, SIZEOF_MATRIX);
+  matrix[15] = 1.0;
 
-  if(x){
-    m[0]=1.0; m[5]=cos(angle); m[6]=sin(angle); m[9]=-sin(angle); m[10]=cos(angle);
-  } else if(y) {
-    m[0]=cos(angle); m[2]=-sin(angle); m[5]=1.0; m[8]=sin(angle); m[10]=cos(angle);
-  } else if(z) {
-    m[0]=cos(angle); m[1]=sin(angle); m[4]=-sin(angle); m[5]=cos(angle); m[10]=1.0;
+  if (x)
+  {
+    matrix[index (0,0)] = 1.0;
+    matrix[index (1,1)] = cos (angle); 
+    matrix[index (1,2)] = sin (angle); 
+    matrix[index (2,1)] = -sin (angle); 
+    matrix[index (2,2)] = cos (angle);
   }
-  multMatrix(m);
+  else if (y) 
+  {
+    matrix[index (0,0)] = cos (angle); 
+    matrix[index (0,2)] = -sin (angle); 
+    matrix[index (1,1)] = 1.0;
+    matrix[index (2,0)] = sin (angle);
+    matrix[index (2,2)] = cos (angle);
+  } 
+  else if (z) 
+  {
+    matrix[index (0,0)] = cos (angle); 
+    matrix[index (0,1)] = sin (angle);
+    matrix[index (1,0)] = -sin (angle); 
+    matrix[index (1,1)] = cos (angle);
+    matrix[index (2,2)] = 1.0;
+  }
+
+  // Multiply the matrix with that on the top of the stack.
+  return multMatrix (matrix);
 }
 
-Vertex globalVertex(Vertex a)
+Vertex globalVertex (Vertex a)
 {
-  float *m=mStack->topMatrix;
+  float *matrix = mStack->topMatrix;
   float w;
-  w=a.x*m[3]+a.y*m[7]+a.z*m[11]+m[15];
-  if(w==0)	w=1;
-  return vertex((a.x*m[0]+a.y*m[4]+a.z*m[8]+m[12])/w, (a.x*m[1]+a.y*m[5]+a.z*m[9]+m[13])/w, (a.x*m[2]+a.y*m[6]+a.z*m[10]+m[14])/w);
+  w = a.x * matrix[index(0,3)] + a.y * matrix[index(1,3)] + 
+      a.z * matrix[index(2,3)] + matrix[index(3,3)];
+
+  if (w == 0)
+  {
+    w = 1;
+  }
+
+  return vertex ((a.x * matrix[index(0,0)] + a.y * matrix[index(1,0)] +
+                  a.z * matrix[index(2,0)] + matrix[index(3,0)]) / w,
+                 (a.x * matrix[index(0,1)] + a.y * matrix[index(1,1)] + 
+                  a.z * matrix[index(2,1)] + matrix[index(3,1)]) / w, 
+                 (a.x * matrix[index(0,2)] + a.y * matrix[index(1,2)] +
+                  a.z * matrix[index(2,2)] + matrix[index(3,2)]) / w);
 }
   
-Vertex eyeVertex(Vertex a)
+Vertex eyeVertex (Vertex a)
 {
-  float *m=pStack->topMatrix;
+  float *matrix = pStack->topMatrix;
   float w;
-  w=a.x*m[3]+a.y*m[7]+a.z*m[11]+m[15];
-  if(w==0)	w=1;
-  return vertex((a.x*m[0]+a.y*m[4]+a.z*m[8]+m[12])/w, (a.x*m[1]+a.y*m[5]+a.z*m[9]+m[13])/w, (a.x*m[2]+a.y*m[6]+a.z*m[10]+m[14])/w);
+  w = a.x * matrix[index(0,3)] + a.y * matrix[index(1,3)] + 
+      a.z * matrix[index(2,3)] + matrix[index(3,3)];
+
+  if(w == 0)
+  {
+    w = 1;
+  }
+
+  return vertex((a.x * matrix[index(0,0)] + a.y * matrix[index(1,0)] + 
+                 a.z * matrix[index(2,0)] + matrix[index(3,0)]) / w,
+                (a.x * matrix[index(0,1)] + a.y * matrix[index(1,1)] +
+                 a.z * matrix[index(2,1)] + matrix[index(3,1)]) / w, 
+                (a.x * matrix[index(0,2)] + a.y * matrix[index(1,2)] +
+                 a.z * matrix[index(2,2)] + matrix[index(3,2)]) / w);
 }
 
